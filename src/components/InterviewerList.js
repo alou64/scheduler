1a707bafe9e75e4e7f706eddf7c1e2b77e346c70
alou <01008@protonmail.com>
2022-02-24 09:53:53 -0800

    refactored components

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -4,20 +4,15 @@ import './InterviewerList.scss';
 
 
 export default function InterviewerList(props) {
-  const parsedInterviewers = props.interviewers.map(interviewer => (
-    <InterviewerListItem key={interviewer.id}
+  const interviewers = props.interviewers.map((interviewer) => {
+  return (
+    <InterviewerListItem
+      key={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
       selected={interviewer.id === props.interviewer}
-      setInterviewer={props.setInterviewer}
+      setInterviewer={() => onChange(interviewer.id)}
     />
-    )
   );
-
-  return (
-    <section className="interviewers">
-      <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list">{ parsedInterviewers }</ul>
-    </section>
-  );
+  });
 };
--- stories/index.js
+++ stories/index.js
@@ -65,13 +65,13 @@ storiesOf("DayList", module)
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
   })
   .add("Monday", () => (
-    <DayList days={days} day={"Monday"} setDay={action("setDay")} />
+    <DayList days={days} value={"Monday"} onChange={action("setDay")} />
   ))
   .add("Tuesday", () => (
-    <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
+    <DayList days={days} value={"Tuesday"} onChange={action("setDay")} />
   ))
   .add("Wednesday", () => (
-      <DayList days={days} day={"Wednesday"} setDay={action("setDay")} />
+    <DayList days={days} value={"Wednesday"} onChange={action("setDay")} />
   ));
 
 
@@ -134,8 +134,9 @@ storiesOf("InterviewerList", module)
     />
   ))
   .add("Clickable", () => (
-    <InterviewerList
-      interviewers={interviewers}
-      setInterviewer={action("setInterviewer")}
+    <InterviewerListItem
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      setInterviewer={() => action("setInterviewer")(interviewer.id)}
     />
 ));
