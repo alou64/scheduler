218f38791bf35475c8baeb827718ea41bb82cf80
alou <01008@protonmail.com>
2022-02-26 12:41:02 -0800

    add selector function

---
+++ src/helpers/selectors.js
@@ -0,0 +1,19 @@
+export function getAppointmentsForDay(state, day) {
+  const getApptIds = () => {
+    try {
+      return state.days.filter(item => item.name === day)[0].appointments;
+    }
+    catch {
+      return [];
+    }
+  };
+
+  const ans = getApptIds();
+
+  for (let i = 0; i < ans.length; i++) {
+    ans.push(state.appointments[String(ans[0])]);
+    ans.shift()
+  }
+
+  return ans;
+};
