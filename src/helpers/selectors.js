3f87e1fe5bd424aae7ae3cfd9d246edde7e9ef7f
alou <01008@protonmail.com>
2022-02-26 15:12:22 -0800

    fetch appointment data from api

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,4 @@
-export function getAppointmentsForDay(state, day) {
+export default function getAppointmentsForDay(state, day) {
   const getApptIds = () => {
     try {
       return state.days.filter(item => item.name === day)[0].appointments;
